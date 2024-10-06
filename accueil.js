24ec8fa80387e845c4131c1a13cbacb51117a891
Hippopcorn <elsa.varache@gmail.com>
2024-10-06 21:48:41 +0200

    ajout du formulaire pour le nom de la ferme

---
+++ accueil.js
@@ -0,0 +1,9 @@
+//Récupérer le nom de la ferme
+buttonFarmName = document.querySelector(".buttonName");
+console.log(buttonFarmName)
+NameFarm = document.querySelector(".farmName");
+
+buttonFarmName.addEventListener("click", () => {
+    event.preventDefault
+    console.log(NameFarm)
+})
