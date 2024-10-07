f39e21e471486e61e5bfa3c2536bd8b797ba44ad
Elsa V. <150471502+Hippopcorn@users.noreply.github.com>
2024-10-07 15:43:13 +0200

    Update accueil.js

--- accueil.js
+++ accueil.js
@@ -1,8 +1,8 @@
 //Récupérer le nom de la ferme
-formulaireFarmName = document.querySelector(".FormNameFarm");
+const formulaireFarmName = document.querySelector(".formNameFarm");
 
 formulaireFarmName.addEventListener("submit", function (event) {
     event.preventDefault();
-    NameChoice = event.target.querySelector("[name="farmName]").value  
+    const NameChoice = event.target.querySelector("[name=farmName]").value  
     console.log(NameChoice);
 })
